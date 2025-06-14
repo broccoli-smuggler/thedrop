bc70310b4d4208da4a0d98b4e63b782c9ed30197
Mike Wessling <mwessling@gmail.com>
2025-06-14 18:43:50 +0200

    push it

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 #include <Audio.h>
 #include <FS.h>
+#include <SD.h>
+#include <SPI.h>
 #include <SPIFFS.h>
 #include <ezButton.h>
+
+// Our files
+#include "sd_card.h"
 
 #define FORMAT_SPIFFS_IF_FAILED true
 #define I2S_DOUT 14
@@ -16,18 +21,30 @@ void interupt_update_flywheel_ripems();
 void calc_flywheel_ripems();
 void update_audio();
 void update_audio_speed();
+void update_audio_volume();
+void fire();
+
+// Fire
+const int output_RPM_led = 2;
 
 // Audio
 Audio audio;
 unsigned long last_speed_adjustment;
-const char *audio_sample = "r.wav";
+const char *audio_sample = "/got.wav";
+const char *audio_sample_2 = "/got_roar.wav";
+const char *current_audio_sample = audio_sample;
+
 long max_rpm = 600;
 int diff = 10;
 unsigned long last_pulse_test;
 bool update_on_loop = true;
 bool update_on_pulse = false;
-bool test_pulse = false;
+bool test_pulse = true;
+const int audio_loop_timer = 1000;
+unsigned long next_loop_update;
 
+
+//RPM
 const unsigned int threshold = 100;
 
 const int idle_time = 2500;
@@ -45,7 +62,7 @@ int last_flywheel, flywheel = 0;
 unsigned int sensor_prev = 0;
 
 // RPM light
-const int output_RPM_led = 2;
+const int output_fire = 2;
 
 int last_pulse, pulse = 0;
 
@@ -60,10 +77,12 @@ void setup()
     attachInterrupt(input_ir_sensor_pin, interupt_update_flywheel_ripems, FALLING);
     pinMode(output_RPM_led, OUTPUT);
 
+    setup_sd();
+
     last_pulse_test = millis();
+    next_loop_update = millis();
+
     esp_sleep_enable_ext0_wakeup(magnetic_switch, 1);
-    // Serial.flush();                                                // Waits for the transmission of outgoing serial data to complete.
-    // esp_deep_sleep_start();
 
     last_led_time = millis();
     digitalWrite(output_RPM_led, HIGH);
@@ -75,7 +94,7 @@ void setup()
     }
 
     audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
-    audio.connecttoFS(SPIFFS, audio_sample);
+    audio.connecttoFS(SD, current_audio_sample);
     audio.setFileLoop(true);
 }
 
@@ -98,9 +117,9 @@ void loop()
     if (test_pulse && now - last_pulse_test > 1000)
     {
         if (flywheel_rpm > max_rpm)
-            diff = -10;
+            diff = -15;
         if (flywheel_rpm <= 0)
-            diff = 10;
+            diff = 15;
 
         flywheel_rpm += diff;
         last_pulse_test = now;
@@ -113,11 +132,49 @@ void loop()
     update_audio();
 }
 
+void fire()
+{
+    Serial.println("Firing!");
+    digitalWrite(output_RPM_led, LOW);
+    digitalWrite(output_RPM_led, HIGH);
+    Serial.println("Done firing.");
+}
+
 void audio_info(const char *info)
 {
-    Serial.println(info);
-    if (update_on_loop)
-        update_audio_speed();
+    if (millis() > next_loop_update)
+    {
+        unsigned long now = millis();
+        next_loop_update = now + audio_loop_timer;
+
+        const char *prior_sample = current_audio_sample;
+
+        // Update audio sample based on flywheel RPM
+        if (flywheel_rpm > 400)
+        {
+            current_audio_sample = audio_sample_2;
+        } else 
+        {
+            current_audio_sample = audio_sample;
+        }
+
+        // On fast enough and ending
+        Serial.println(prior_sample);
+        Serial.println(flywheel_rpm);
+        if (prior_sample == audio_sample_2 && flywheel_rpm >= 400)
+        {
+            fire();
+        }
+
+        if (current_audio_sample != prior_sample)
+        {
+            audio.connecttoFS(SD, current_audio_sample);
+            audio.setFileLoop(true);
+        }
+
+        if (update_on_loop)
+            update_audio_speed();
+    }
 }
 
 void update_audio()
@@ -125,15 +182,23 @@ void update_audio()
     unsigned long now = millis();
 
     audio.loop();
+    update_audio_volume();
     last_speed_adjustment = now;
+}
+
+
+void update_audio_volume()
+{
+    long result_volume = map(flywheel_rpm, 0, 600, 0, 21);
+
+    audio.setVolume(uint8_t(result_volume));
 }
 
 void update_audio_speed()
 {
-    static long min_speed = 0.4 * 100;
-    static long max_speed = 2.0 * 100;
+    static long min_speed = 0.8 * 100;
+    static long max_speed = 1.3 * 100;
     long result_speed = map(flywheel_rpm, 0, 600, min_speed, max_speed);
-
     audio.audioFileSeek(float(result_speed / 100.0));
 }
 
@@ -163,13 +228,14 @@ void update_flywheel_ripems(unsigned long now)
 
 void interupt_update_flywheel_ripems()
 {
-        flywheel_falling_edge_time = millis();
+    flywheel_falling_edge_time = millis();
 }
 
 void calc_flywheel_ripems()
 {
-    if(flywheel_falling_edge_time > prev_flywheel_falling_edge_time){
-        flywheel_rpm = 60000.0 / (flywheel_falling_edge_time-prev_flywheel_falling_edge_time);
+    if (flywheel_falling_edge_time > prev_flywheel_falling_edge_time)
+    {
+        flywheel_rpm = 60000.0 / (flywheel_falling_edge_time - prev_flywheel_falling_edge_time);
         prev_flywheel_falling_edge_time = flywheel_falling_edge_time;
     }
 }
